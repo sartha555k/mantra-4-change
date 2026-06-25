552011cb63bd9aa0f03653a13fb379419a6418ae
sartha555k <sartha555k@gmail.com>
2026-06-25 16:57:28 +0530

    Fix CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(
     cors({
         origin: [
-            "http://localhost:5173",
+            "http://localhost:5175",
             "https://mantra-4-change.vercel.app",
         ],
         credentials: true,
